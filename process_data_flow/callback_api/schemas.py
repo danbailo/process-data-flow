bbb7274f1a0ba1078ed0b8e8e5aa7819e27debd4
Daniel Bailo <danbailoufms@gmail.com>
2024-06-15 16:34:34 -0300

    feat: added min_length in observation property

--- process_data_flow/callback_api/schemas.py
+++ process_data_flow/callback_api/schemas.py
@@ -6,7 +6,7 @@ class ProductSchema(BaseModel):
     id: UUID4
     name: str
     price: float = Field(..., gt=0, lt=1000)
-    observation: str | None
+    observation: str | None = Field(min_length=80)
 
 
 class ProductFactory(ModelFactory[ProductSchema]):
