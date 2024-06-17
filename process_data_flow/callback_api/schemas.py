e9f522bb5def7489d8a32d49b2cf6fe41853f906
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 14:53:46 -0300

    refactor: changed length of fields

--- process_data_flow/callback_api/schemas.py
+++ process_data_flow/callback_api/schemas.py
@@ -4,9 +4,9 @@ from pydantic import UUID4, BaseModel, Field
 
 class ProductSchema(BaseModel):
     id: UUID4
-    name: str
+    name: str = Field(..., min_length=10)
     price: float = Field(..., gt=0, lt=1000)
-    observation: str | None = Field(min_length=80)
+    observation: str | None = Field(min_length=16)
 
 
 class ProductFactory(ModelFactory[ProductSchema]):
