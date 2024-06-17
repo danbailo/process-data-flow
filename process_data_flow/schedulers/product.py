9e44a5740e0ee9e3a0504495add31617ba263014
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 14:55:33 -0300

    refactor: removed logger from scheduler

--- process_data_flow/schedulers/product.py
+++ process_data_flow/schedulers/product.py
@@ -2,12 +2,10 @@ import time
 
 import schedule
 
-from process_data_flow.commons.logger import LoggerFactory
 from process_data_flow.services.product import SendProductsToRabbitService
 
 if __name__ == '__main__':
-    logger = LoggerFactory.new()
-    send_products_to_rabbit = SendProductsToRabbitService(logger=logger)
+    send_products_to_rabbit = SendProductsToRabbitService()
 
     schedule.every(10).seconds.do(send_products_to_rabbit.execute)
 
