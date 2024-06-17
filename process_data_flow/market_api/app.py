8ae00acb5f3045efa837c4bf5b9feb08f95b6e6e
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 16:22:33 -0300

    feat: endpoint oto create product

--- process_data_flow/market_api/app.py
+++ process_data_flow/market_api/app.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
+from uuid import uuid4
 
 import uvicorn
-from fastapi import Depends, FastAPI, Query
+from fastapi import Depends, FastAPI, HTTPException, Query, status
 from pydantic import UUID4
 from sqlmodel import Session, func, select
 
@@ -9,6 +10,7 @@ from process_data_flow.commons.api import BuildListResponse
 from process_data_flow.commons.logger import Logger, LoggerFactory
 from process_data_flow.market_api.database import get_session, init_db
 from process_data_flow.market_api.models import ProductModel
+from process_data_flow.schemas import ProductIn, ProductOut
 
 _logger: Logger = LoggerFactory.new()
 
@@ -45,10 +47,21 @@ async def get_products(
     return to_return
 
 
-# @app.post('/product')
-# async def create_product(
-#     body:
-# )
+@app.post('/product', response_model=ProductOut, status_code=status.HTTP_201_CREATED)
+async def create_product(product: ProductIn, session: Session = Depends(get_session)):
+    product_id = uuid4()
+    product_from_db = session.exec(
+        select(ProductModel).where(ProductModel.id == product_id)
+    ).first()
+    if product_from_db:
+        raise HTTPException(
+            status=status.HTTP_409_CONFLICT,
+            detail=f'Item with ID {product_from_db.id} already exists!',
+        )
+    new_product = ProductModel(id=product_id, **product.model_dump())
+    session.add(new_product)
+    session.commit()
+    return new_product
 
 
 @app.get('/health')
