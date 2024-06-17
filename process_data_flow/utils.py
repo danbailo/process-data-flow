6c848102b8754c67cbf3651bf326ff620aa65f63
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 15:11:51 -0300

    refactor: utils

--- process_data_flow/utils.py
+++ process_data_flow/utils.py
@@ -3,9 +3,12 @@ import random
 from process_data_flow.callback_api.schemas import ProductFactory, ProductSchema
 
 
+def generate_fake_products(size: int = 1) -> list[ProductSchema]:
+    return ProductFactory.batch(size)
+
 def generate_random_data() -> list[ProductSchema]:
     should_return_value = random.choice([True, False])
     if not should_return_value:
         return []
-    len_items = random.randint(1, 10)
-    return ProductFactory.batch(len_items)
+    size = random.randint(1, 10)
+    return generate_fake_products(size)
