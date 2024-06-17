d7dd57c239b3945f608571330c55ae09aea82a9d
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 14:55:44 -0300

    feat: format product service

--- process_data_flow/services/product.py
+++ process_data_flow/services/product.py
@@ -4,6 +4,7 @@ import json
 import httpx
 from tenacity import retry, stop_after_attempt, wait_fixed
 
+from process_data_flow.callback_api.schemas import ProductSchema
 from process_data_flow.commons.logger import Logger, LoggerFactory
 from process_data_flow.commons.rabbitmq_client import RabbitMQClient
 from process_data_flow.commons.tenacity import warning_if_failed
@@ -66,3 +67,30 @@ class SendProductsToRabbitService:
         self.logger.info(
             'Products sent with successfully!', data=dict(len_products=len(products))
         )
+
+
+class FormatProductService:
+    def __init__(self, logger: Logger = LoggerFactory.new()):
+        self.logger = logger
+
+    def _format_product(self, product: dict) -> dict:
+        product['name'] = f"{product['name'][:5]} {product['name'][5:]}"
+        product['price'] = round(product['price'], 2)
+        product['observation'] = (
+            product['observation'][:16] * 2 if product['observation'] else None
+        )
+        return product
+
+    def _load_product_from_rabbitmq(self, product_from_queue: bytes) -> ProductSchema:
+        product_dict = json.loads(product_from_queue.decode())
+        return ProductSchema(**product_dict)
+
+    def execute(self, product_from_queue: bytes) -> ProductSchema:
+        self.logger.info('Executing Format Product Service...')
+
+        product = self._load_product_from_rabbitmq(product_from_queue)
+        product_id = product.id
+
+        to_return = self._format_product(product.model_dump(exclude={'id'}))
+        self.logger.info('Product formatted!', data=dict(product_id=product_id))
+        return ProductSchema(id=product_id, **to_return)
