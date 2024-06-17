9ba9756a3e3b075a1a1ed9bfeec641de96604c48
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 16:17:41 -0300

    feat: product in/product out

--- process_data_flow/schemas.py
+++ process_data_flow/schemas.py
@@ -2,12 +2,15 @@ from polyfactory.factories.pydantic_factory import ModelFactory
 from pydantic import UUID4, BaseModel, Field
 
 
-class ProductSchema(BaseModel):
-    id: UUID4
+class ProductIn(BaseModel):
     name: str = Field(..., min_length=10)
     price: float = Field(..., gt=0, lt=1000)
     observation: str | None = Field(min_length=16)
 
 
-class ProductFactory(ModelFactory[ProductSchema]):
-    __model__ = ProductSchema
+class ProductOut(ProductIn):
+    id: UUID4
+
+
+class ProductFactory(ModelFactory[ProductOut]):
+    __model__ = ProductOut
