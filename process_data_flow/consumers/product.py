231e77d55ef99be59a8a296228b6cfbab98e0af0
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 21:44:03 -0300

    feat: product consumer

--- process_data_flow/consumers/product.py
+++ process_data_flow/consumers/product.py
@@ -1,78 +1,30 @@
-from abc import ABC, abstractmethod
 
+import asyncio
+
+import httpx
 from pika.channel import Channel
 from pika.spec import Basic, BasicProperties
-from pydantic import BaseModel
+from tenacity import retry, stop_after_attempt, wait_fixed
 
-from process_data_flow.commons.logger import Logger, LoggerFactory
-from process_data_flow.commons.rabbitmq_client import RabbitMQClient
+from process_data_flow.commons.rabbitmq.consumer import (
+    RabbitMQConsumer,
+    RabbitMQConsumerOptions,
+)
+from process_data_flow.commons.tenacity import warning_if_failed
 from process_data_flow.services.product import FormatProductService
-from process_data_flow.settings import PRODUCT_CONSUMER_QUEUE
+from process_data_flow.settings import (
+    MARKET_API_URL,
+    MARKET_QUERY_EXCHANGE,
+    MARKET_QUERY_KEY,
+    PRODUCT_CONSUMER_QUEUE,
+    RETRY_AFTER_SECONDS,
+    RETRY_ATTEMPTS,
+)
 
 
-class RabbitMQConsumerOptions(BaseModel):
-    queue: str
-    auto_ack: bool = False
-    exclusive: bool = False
-    # consumer_tag=None
-    arguments: dict = None
-
-
-class RabbitMQConsumer(ABC):
-    def __init__(
-        self, options: RabbitMQConsumerOptions, logger: Logger = LoggerFactory.new()
-    ):
-        self.client = RabbitMQClient()
-        self.logger = logger
-        self.client.channel.basic_qos(prefetch_count=1)
-
-        self.options = options
-
-    @abstractmethod
-    def _handle_message(
-        self,
-        ch: Channel,
-        method: Basic.Deliver,
-        properties: BasicProperties,
-        body: bytes,
-        exc: Exception | None = None,
-    ):
-        pass
-
-    @abstractmethod
-    def _execute(
-        self,
-        ch: Channel,
-        method: Basic.Deliver,
-        properties: BasicProperties,
-        body: bytes,
-    ):
-        pass
-
-    def _callback(
-        self,
-        ch: Channel,
-        method: Basic.Deliver,
-        properties: BasicProperties,
-        body: bytes,
-    ):
-        self.logger = self.logger.new(
-            monitoring=dict(queue=self.options.queue, body=body.decode())
-        )
-        try:
-            self._execute(ch, method, properties, body)
-            self._handle_message(ch, method, properties, body)
-            self.logger.debug('Error when consuming message!')
-        except Exception as exc:
-            self.logger.debug('Error when consuming message!')
-            self._handle_message(ch, method, properties, body, exc)
-
-    def consume(self):
-        self.logger.info('Consuming queue', monitoring=dict(queue=self.options.queue))
-
-        options = self.options.model_dump(exclude_none=True)
-        self.client.channel.basic_consume(**options, on_message_callback=self._callback)
-        self.client.channel.start_consuming()
+class ItemAlreadyExists(Exception):
+    def __init__(self, message: str = 'Item already exists in database!') -> None:
+        super().__init__(message)
 
 
 class ProductConsumer(RabbitMQConsumer):
@@ -82,20 +34,40 @@ class ProductConsumer(RabbitMQConsumer):
         )
         super().__init__(options=options)
 
-    def _handle_message(
-        self,
-        ch: Channel,
-        method: Basic.Deliver,
-        properties: BasicProperties,
-        body: bytes,
-        exc: Exception | None = None,
-    ):
-        if exc is None:
-            self.client.channel.basic_ack(delivery_tag=method.delivery_tag)
-            self.logger.info('Message consumed with sucessfully!')
-        else:
-            self.client.channel.basic_nack(delivery_tag=method.delivery_tag)
-            self.logger.exception('Error when consuming message!')
+    @retry(
+        reraise=True,
+        stop=stop_after_attempt(RETRY_ATTEMPTS),
+        wait=wait_fixed(RETRY_AFTER_SECONDS),
+        before=warning_if_failed,
+    )
+    async def _get_product(self, id: str):
+        url = MARKET_API_URL + f'/product/{id}'
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+
+        data = response.json()
+        if isinstance(data, dict) and ('not exists' in data.get('detail', '')):
+            return response
+
+        response.raise_for_status()
+
+        return response
+
+    @retry(
+        reraise=True,
+        stop=stop_after_attempt(RETRY_ATTEMPTS),
+        wait=wait_fixed(RETRY_AFTER_SECONDS),
+        before=warning_if_failed,
+    )
+    async def _create_product(self, data: dict):
+        url = MARKET_API_URL + '/product'
+
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url, json=data)
+        response.raise_for_status()
+
+        return response
 
     def _execute(
         self,
@@ -106,6 +78,19 @@ class ProductConsumer(RabbitMQConsumer):
     ):
         format_product_service = FormatProductService(self.logger)
         product = format_product_service.execute(body)
+
+        response = asyncio.run(self._get_product(product.id))
+
+        if response.status_code == 200:
+            raise ItemAlreadyExists()
+
+        asyncio.run(self._create_product(product.model_dump(mode='json')))
+
+        self.client.send_message(
+            exchange=MARKET_QUERY_EXCHANGE,
+            routing_key=MARKET_QUERY_KEY,
+            body=product.id.hex,
+        )
 
 
 if __name__ == '__main__':
