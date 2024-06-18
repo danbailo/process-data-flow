a2699b2ca4f192c786292e2edec478d311ec7d5b
Daniel Bailo <danbailoufms@gmail.com>
2024-06-17 22:03:24 -0300

    feat[wip]: market query consumer

---
+++ process_data_flow/consumers/market_query.py
@@ -0,0 +1,46 @@
+
+import asyncio
+
+import httpx
+from pika.channel import Channel
+from pika.spec import Basic, BasicProperties
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+from process_data_flow.commons.rabbitmq.consumer import (
+    RabbitMQConsumer,
+    RabbitMQConsumerOptions,
+)
+from process_data_flow.commons.tenacity import warning_if_failed
+from process_data_flow.services.product import FormatProductService
+from process_data_flow.settings import (
+    MARKET_QUERY_QUEUE,
+)
+
+
+class MarketQueryConsumer(RabbitMQConsumer):
+    def __init__(self) -> None:
+        options = RabbitMQConsumerOptions(
+            queue=MARKET_QUERY_QUEUE,
+            requeue=False
+        )
+        super().__init__(options=options)
+
+    def _execute(
+        self,
+        ch: Channel,
+        method: Basic.Deliver,
+        properties: BasicProperties,
+        body: bytes,
+    ):
+        breakpoint()
+        raise Exception('foo')
+
+        # self.client.send_message(
+        #     exchange=MARKET_QUERY_EXCHANGE,
+        #     routing_key=MARKET_QUERY_KEY,
+        #     body=product.id.hex,
+        # )
+
+
+if __name__ == '__main__':
+    MarketQueryConsumer().consume()
