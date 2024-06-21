147a4e23bbb7ed1dc148c7716802e17fd974e87e
Daniel Bailo <danbailoufms@gmail.com>
2024-06-20 21:31:38 -0300

    hotfix: mypy types

--- process_data_flow/commons/requests.py
+++ process_data_flow/commons/requests.py
@@ -28,6 +28,7 @@ class MethodRequestEnum(StrEnum):
 async def make_async_request(
     method: MethodRequestEnum,
     url: str,
+    *,
     data: dict | None = None,
     params: dict | None = None,
     headers: dict | None = None,
@@ -40,6 +41,8 @@ async def make_async_request(
     _logger.debug(
         'Making async request...',
         data=dict(
+            method=method,
+            url=url,
             data=data,
             params=params,
             headers=headers,
@@ -50,7 +53,7 @@ async def make_async_request(
         ),
     )
 
-    if authorization:
+    if authorization and isinstance(headers, dict):
         headers['Authorization'] = authorization
 
     async with AsyncClient() as client:
@@ -78,6 +81,7 @@ async def make_async_request(
 def make_request(
     method: MethodRequestEnum,
     url: str,
+    *,
     data: dict | None = None,
     params: dict | None = None,
     headers: dict | None = None,
@@ -90,6 +94,8 @@ def make_request(
     _logger.debug(
         'Making request...',
         data=dict(
+            method=method,
+            url=url,
             data=data,
             params=params,
             headers=headers,
@@ -100,7 +106,7 @@ def make_request(
         ),
     )
 
-    if authorization:
+    if authorization and isinstance(headers, dict):
         headers['Authorization'] = authorization
 
     response = httpx.request(
