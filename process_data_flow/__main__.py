22f361f3affe428d4505ceec42022b288feceeb2
Daniel Bailo <danbailoufms@gmail.com>
2024-06-25 10:27:55 -0300

    feat: consumer cli

--- process_data_flow/__main__.py
+++ process_data_flow/__main__.py
@@ -5,6 +5,9 @@ from typer import Option, Typer
 
 from process_data_flow.commons.logger import Logger, LoggerFactory
 from process_data_flow.commons.mp_scheduler import MPScheduler
+from process_data_flow.commons.rabbitmq.consumer import RabbitMQConsumerOptions
+from process_data_flow.consumers.market_query import MarketQueryConsumer
+from process_data_flow.consumers.product import ProductConsumer
 from process_data_flow.services.product import SendExtractDataToProductQueueService
 
 logger: Logger = LoggerFactory.new()
@@ -13,6 +16,7 @@ app = Typer()
 
 api = Typer()
 scheduler = Typer()
+consumer = Typer()
 
 
 @api.command()
@@ -42,7 +46,38 @@ def send_extract_data_to_rabbitmq(seconds: int = Option(default=3600)):
         time.sleep(1)
 
 
+@consumer.command()
+def product(
+    auto_ack: bool = Option(default=False),
+    exclusive: bool = Option(default=False),
+    requeue: bool = Option(default=True),
+):
+    options = RabbitMQConsumerOptions(
+        auto_ack=auto_ack,
+        exclusive=exclusive,
+        requeue=requeue,
+    )
+    product_consumer = ProductConsumer(options)
+    product_consumer.consume()
+
+
+@consumer.command()
+def market_query(
+    auto_ack: bool = Option(default=False),
+    exclusive: bool = Option(default=False),
+    requeue: bool = Option(default=True),
+):
+    options = RabbitMQConsumerOptions(
+        auto_ack=auto_ack,
+        exclusive=exclusive,
+        requeue=requeue,
+    )
+    market_query_consumer = MarketQueryConsumer(options=options)
+    market_query_consumer.consume()
+
+
 app.add_typer(api, name='api')
 app.add_typer(scheduler, name='scheduler')
+app.add_typer(consumer, name='consumer')
 
 app()
